414af50070b886d629562dcc0a7bf50527fc3e33
Bozhidar Vangelov <bozhidar.vangelov@gmail.com>
2025-11-23 09:46:16 +0200

    fix(api): add prisma pg adapter

--- apps/api/src/core/infrastructure/database/prisma/prisma.service.ts
+++ apps/api/src/core/infrastructure/database/prisma/prisma.service.ts
@@ -5,6 +5,8 @@ import {
   OnModuleDestroy,
   OnModuleInit,
 } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import * as process from 'process';
 
 @Injectable()
 export class PrismaService
@@ -12,6 +14,18 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('DATABASE_URL environment variable not set.');
+    }
+
+    const adapter = new PrismaPg({ connectionString });
+
+    super({ adapter });
+  }
 
   async onModuleInit() {
     await this.$connect();
