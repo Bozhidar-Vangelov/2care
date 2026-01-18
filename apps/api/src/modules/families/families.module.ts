02d29a9621714ff0bef7ff1748e3d45ee6b20276
Bozhidar Vangelov <bozhidar.vangelov@gmail.com>
2026-01-18 22:17:57 +0200

    feat(api): implement create, list, update and delete families #29

--- apps/api/src/modules/families/families.module.ts
+++ apps/api/src/modules/families/families.module.ts
@@ -1,8 +1,10 @@
+import { PrismaModule } from '@/core/infrastructure/database/prisma/prisma.module';
 import { Module } from '@nestjs/common';
-import { FamiliesService } from './families.service';
 import { FamiliesController } from './families.controller';
+import { FamiliesService } from './families.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [FamiliesController],
   providers: [FamiliesService],
 })
